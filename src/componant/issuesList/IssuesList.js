37758ddeb7bcdc0c04b508fe4bf62ddce7d87c8e
Shafin The Developer <sahariar350@gmail.com>
2021-06-04 17:14:12 +0600

    done

--- src/componant/issuesList/IssuesList.js
+++ src/componant/issuesList/IssuesList.js
@@ -8,14 +8,14 @@ function IssuesList() {
     return (
         <div id="issuesList">
             {issues.map(issue => (
-                <div class="well">
+                <div className="well">
                     <h6>Issue ID: {issue.issueID} </h6>
                     <p><span className={issue.issueClosed ? "label label-danger" : "label label-info"}> {issue.issueClosed ? "Close" : "Open"}  </span></p>
                     <h3> {issue.issueDescription} </h3>
-                    <p><span class="glyphicon glyphicon-time"> {issue.issueSeverity}</span> </p>
-                    <p><span class="glyphicon glyphicon-user"> {issue.issueAssignedTo}</span> </p>
-                    <button onClick={() => dispatch(closeIssue(issue.issueID))} class="btn btn-warning">Close</button>
-                    <button onClick={() => dispatch(deleteIssue(issue.issueID))} class="btn btn-danger">Delete</button>
+                    <p><span className="glyphicon glyphicon-time"> {issue.issueSeverity}</span> </p>
+                    <p><span className="glyphicon glyphicon-user"> {issue.issueAssignedTo}</span> </p>
+                    <button onClick={() => dispatch(closeIssue(issue.issueID))} className="btn btn-warning">Close</button>
+                    <button onClick={() => dispatch(deleteIssue(issue.issueID))} className="btn btn-danger">Delete</button>
                 </div>
             ))
             }
